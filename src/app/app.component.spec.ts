4cb285cc27a47c7737b523fd0fbf27e56aacf910
José Eduardo Trindade E Marques <edu.temarques@gmail.com>
2023-08-23 21:34:16 -0300

    test: impl. navbar unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,12 +2,13 @@ import { TestBed } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { NavbarModule } from './shared/components/navbar/navbar.module';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
-      imports: [RouterModule],
+      imports: [RouterModule, NavbarModule],
     }).compileComponents();
   });
 
@@ -21,5 +22,12 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('person-register-frontend');
+  });
+
+  it('should have a navbar', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const dom = fixture.nativeElement as HTMLElement;
+
+    expect(dom.getElementsByTagName('edv-navbar')).toBeTruthy();
   });
 });
--- src/app/shared/components/navbar/navbar.component.spec.ts
+++ src/app/shared/components/navbar/navbar.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { NavbarComponent } from './navbar.component';
 
@@ -9,6 +10,7 @@ describe('NavbarComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [NavbarComponent],
+      imports: [MatToolbarModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(NavbarComponent);
@@ -18,5 +20,25 @@ describe('NavbarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render a mat-toolbar component', () => {
+    expect(document.getElementsByTagName('mat-toolbar')).toBeTruthy();
+  });
+
+  it('should render an app title ', () => {
+    expect(document.getElementById('title')).toBeTruthy();
+  });
+
+  it('should render an app subtitle ', () => {
+    expect(document.getElementById('subtitle')).toBeTruthy();
+  });
+
+  it('should title be Endeavour', () => {
+    expect(document.getElementById('title')?.innerText).toBe('Endeavour');
+  });
+
+  it('should subtitle be Person Control', () => {
+    expect(document.getElementById('subtitle')?.innerText).toBe('Person Control');
   });
 });
